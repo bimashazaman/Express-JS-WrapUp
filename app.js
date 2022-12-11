3d7755a4f1622316c10e384c62b8cb4821e23304
bimashazaman <bimashazaman1234.com>
2022-12-12 01:14:06 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const express = require('express')
+
+const app = express()
+
+const server = http.createServer(app)
+
+server.listen(3000, () => {
+  console.log('server is running on port 3000')
+})
