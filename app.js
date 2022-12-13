99d773c1fc50e07e2aadc666a8dca4469bbcd915
bimashazaman <bimashazaman1234.com>
2022-12-13 21:51:00 +0600

    first commit

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const path = require('path');
+/* A built-in module that helps us to work with paths. */
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const errorController = require('./controllers/error')
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const app = express()
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
+/* Telling the server to use the bodyParser middleware to parse the body of the request. */
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+/* Telling the server to use the adminRoutes and shopRoutes for the /admin and /shop routes
+respectively. */
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.listen(3000);
+app.use(errorController.get404)
+
+app.listen(3000)
