c4b8061d7bccddba093b311a5a891194b36f4fa7
bimashazaman <bimashazaman1234.com>
2022-12-15 00:26:11 +0600

    New

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-/* A built-in module that helps us to work with paths. */
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-/* Telling the server to use the bodyParser middleware to parse the body of the request. */
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-/* Telling the server to use the adminRoutes and shopRoutes for the /admin and /shop routes
-respectively. */
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-app.listen(3000)
+app.listen(3000);
--- models/product.js
+++ models/product.js
@@ -1,57 +1,40 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const products = []
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  /**
-   * The constructor function is a special function that is called when a new object is created
-   * @param t - The title of the book.
-   */
-  constructor(t) {
-    this.title = t
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    )
-    /* Reading the file and then writing the file. */
-    fs.readFile(p, (err, fileContent) => {
-      /* Creating an empty array. */
-      /* Checking if there is an error and if there is not an error, it is parsing the file content. */
-      let products = []
-      if (!err) {
-        products = JSON.parse(fileContent)
-      }
-      /* Pushing the product into the products array. */
-      /* Writing the file. */
-      products.push(this)
-      fs.writeFile(
-        p,
-        JSON.stringify(products),
-
-        (err) => {
-          console.log(err)
-        }
-      )
-    })
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    )
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([])
-      }
-      cb(JSON.parse(fileContent))
-    })
+    getProductsFromFile(cb);
   }
-}
+};
