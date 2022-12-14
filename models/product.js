ad6d55d134d2d9cf3b2f8b0a9acbbca589c4e2be
bimashazaman <bimashazaman1234.com>
2022-12-14 23:10:43 +0600

    New

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const products = []
 
 module.exports = class Product {
@@ -9,18 +12,46 @@ module.exports = class Product {
     this.title = t
   }
 
-  /**
-   * The save function pushes the current object into the products array
-   */
   save() {
-    products.push(this)
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    )
+    /* Reading the file and then writing the file. */
+    fs.readFile(p, (err, fileContent) => {
+      /* Creating an empty array. */
+      /* Checking if there is an error and if there is not an error, it is parsing the file content. */
+      let products = []
+      if (!err) {
+        products = JSON.parse(fileContent)
+      }
+      /* Pushing the product into the products array. */
+      /* Writing the file. */
+      products.push(this)
+      fs.writeFile(
+        p,
+        JSON.stringify(products),
+
+        (err) => {
+          console.log(err)
+        }
+      )
+    })
   }
 
-  /**
-   * It returns the products array
-   * @returns An array of objects
-   */
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    )
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([])
+      }
+      cb(JSON.parse(fileContent))
+    })
   }
 }
